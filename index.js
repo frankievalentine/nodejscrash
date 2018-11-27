f87e65158a082d9d2d1e875a57598e063f31e027
frankievalentine <frankievalentine9@gmail.com>
2018-11-27 12:11:17 -0800

    setup filesystem, server, url modules handling routing

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const json = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((req, res) => {
+  const pathName = url.parse(req.url, true).pathname;
+  console.log(pathName);
+
+  if (pathName === "/products" || pathName === "") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end("This is the PRODUCTS page");
+  } else if (pathName === "/laptop") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end("This is the LAPTOP page");
+  } else {
+    res.writeHead(404, { "Content-type": "text/html" });
+    res.end("URL not found");
+  }
+});
+
+server.listen(1337, "127.0.0.1", () => {
+  console.log("Listening for requests...");
+});
